515abd14f9dee5360f87ba2c64f5fe0b668d0748
Rogers Ntumwa <ntumwar@gmail.com>
2020-12-17 13:25:40 +0400

    set up redux and implemented redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import store from "./store";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
