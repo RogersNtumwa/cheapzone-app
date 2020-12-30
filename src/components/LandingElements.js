bcb3692b8c5d55bb0c31269692ef783e5fc692b4
Rogers Ntumwa <ntumwar@gmail.com>
2020-12-30 17:24:43 +0400

    Added best collections section on the landing page

--- src/components/LandingElements.js
+++ src/components/LandingElements.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const HeaderSection = styled.div`
   display: flex;
-  clip-path: polygon(0 0, 100% 0, 100% 76%, 0% 100%);
+  clip-path: polygon(0 0, 100% 0, 100% 85%, 0% 100%);
   height: 80vh;
   font-size: 1.6rem;
   background-color: #f7f4f0;
+  z-index: 10;
   @media screen and (max-width: 978px) {
     flex-direction: column;
     height: 90vh;
@@ -16,6 +17,7 @@ export const HeaderText = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  padding: 1rem;
   width: 50%;
   h2 {
     font-size: 3.5rem;
@@ -41,5 +43,81 @@ export const HeaderImg = styled.div`
   @media screen and (max-width: 978px) {
     width: 100%;
     margin-top: 1rem;
+  }
+`;
+
+export const Logodiv = styled.div`
+  height: 5rem;
+  transform: translateY(-3.5rem);
+  width: 70%;
+  float: right;
+
+  @media screen and (max-width: 978px) {
+    width: 100%;
+    transform: translateY(0);
+  }
+  .slide {
+    height: 5rem;
+    margin-right: 1rem;
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: contain;
+    }
+  }
+`;
+
+export const BestCollection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 5rem;
+  height: 40rem;
+  width: 100%;
+
+  .sectionheader {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 2rem;
+    height: 20%;
+    .h2 {
+      font-size: 1.8rem;
+      margin-bottom: 0;
+    }
+    p {
+      font-size: 1.2rem;
+    }
+  }
+  .sectionbody {
+    display: flex;
+    height: 80%;
+    .photo {
+      width: 50%;
+      @media screen and (max-width: 978px) {
+        width: 100%;
+      }
+    }
+    .text {
+      width: 50%;
+
+      padding: 1rem 2rem;
+
+      p {
+        font-size: 1.2rem;
+      }
+      button {
+        margin-right: 1rem;
+      }
+      @media screen and (max-width: 978px) {
+        width: 100%;
+      }
+    }
+    @media screen and (max-width: 978px) {
+      flex-direction: column;
+      padding: 0;
+    }
+  }
+  @media screen and (max-width: 978px) {
+    padding: 0;
   }
 `;
--- src/screens/LandingpageScreen.js
+++ src/screens/LandingpageScreen.js
@@ -1,33 +1,130 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-import Sneaker from "..//images/landingShoe.jpg";
+import "react-multi-carousel/lib/styles.css";
+
 import {
   HeaderSection,
   HeaderText,
   HeaderImg,
+  Logodiv,
+  BestCollection,
 } from "../components/LandingElements";
+import { responsive } from "../utils/Carousel";
+import Carousel from "react-multi-carousel";
 
 const LandingpageScreen = () => {
   return (
-    <HeaderSection>
-      <HeaderText>
-        <h2>
-          Leading Sneeker
-          <br /> Store in Town
-        </h2>
-        <p>
-          It is a long established fact that finding genuine and durable
-          sneekers in Town has been a myth. And Now here comes Cheapzone
-        </p>
-        <p>Pick your Sneaker today</p>
-        <Link to="/shop">
-          <button className="btn-large">Shop Now</button>
-        </Link>
-      </HeaderText>
-      <HeaderImg>
-        <img src={Sneaker} alt="Sneaker" className="image" />
-      </HeaderImg>
-    </HeaderSection>
+    <Fragment>
+      <HeaderSection>
+        <HeaderText>
+          <h2>
+            Leading Sneeker
+            <br /> Store in Town
+          </h2>
+          <p>
+            It is a long established fact that finding genuine and durable
+            sneekers in Town has been a myth. And Now here comes Cheapzone
+          </p>
+          <p>Pick your Sneaker today</p>
+          <Link to="/shop">
+            <button className="btn-large">Shop Now</button>
+          </Link>
+        </HeaderText>
+        <HeaderImg>
+          <img
+            src="https://res.cloudinary.com/dlloahxji/image/upload/v1609322229/landingShoe_nkfb4x.jpg"
+            alt="Sneaker"
+            className="image"
+          />
+        </HeaderImg>
+      </HeaderSection>
+      <Logodiv>
+        <Carousel
+          responsive={responsive}
+          autoPlay={true}
+          infinite={true}
+          centerMode={true}
+          className="carausel"
+        >
+          <div className="slide">
+            <img
+              src="https://res.cloudinary.com/dlloahxji/image/upload/v1609322208/puma_jekigi.png"
+              alt="Sneaker1"
+              className=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://res.cloudinary.com/dlloahxji/image/upload/v1609322175/fila_gkdvsb.png"
+              alt="Sneaker2"
+              className=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://res.cloudinary.com/dlloahxji/image/upload/v1609322193/Nike_zku0cb.png"
+              alt="Sneaker3"
+              className=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://res.cloudinary.com/dlloahxji/image/upload/v1609322164/asics_nlh2cp.png"
+              alt="Sneaker4"
+              className=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://res.cloudinary.com/dlloahxji/image/upload/v1609322152/adidas_nfac3f.jpg"
+              alt="Sneaker4"
+              className="image"
+            />
+          </div>
+        </Carousel>
+      </Logodiv>
+      <BestCollection>
+        <div className="sectionheader">
+          <h2 className="h2">Best Collection</h2>
+          <p>
+            E-commerce is massively competitive with so many different marketing
+            tactics nowdays,
+            <br />
+            It's a confusing landscape and it;s hard to know waht's best
+          </p>
+        </div>
+        <div className="sectionbody">
+          <div className="photo">
+            <img
+              src="https://res.cloudinary.com/dlloahxji/image/upload/v1609327583/melvin-buezo_a3pr6a.jpg"
+              alt="image"
+              className="image"
+            />
+          </div>
+          <div className="text">
+            <h2>
+              Summer Collection
+              <br />
+              <span>On Best Fashion Collection Products</span>
+            </h2>
+            <p>
+              Consumers expext to buy now receive tomorrow.
+              <br />
+              Or Buy now and collect from the store tomorrow
+              <ul>
+                <li>-50% on online shopping.</li>
+                <li>50% of online shoppers have used</li>
+                <li>Delivered in less than 48 hours</li>
+              </ul>
+            </p>
+            <Link to="/shop">
+              <button className="btn-large">Shop Now</button>
+            </Link>
+            <span>$47.20</span>
+          </div>
+        </div>
+      </BestCollection>
+    </Fragment>
   );
 };
 
